9bdeaecdc4053dde9ee0bfa6d00c9d4e3751d46f
wilsonchooo <wcho1205@bths.edu>
2019-01-29 21:44:52 -0500

    zzzzz

--- src/SimpleMerge.java
+++ src/SimpleMerge.java
@@ -7,6 +7,57 @@
 public class SimpleMerge {
     public static int[] simpleMerge(int[] arr1, int[] arr2) {
 
+        int size = arr1.length + arr2.length;
+        int[] Array = new int[size];
+        for (int i=0;i<arr1.length;i++)
+        {
+            Array[i] = arr1[i];
+        }
+
+
+       for (int j=0; j<arr2.length;j++)
+        {
+            Array[j+arr1.length] = arr2[j];
+        }
+
+        quickSort(Array,0,Array.length-1);
+return Array;
+
+
+    }
+
+    public static void quickSort(int[] arr, int left, int right)
+    {
+        if (left<right)
+        {
+            int pivot = Partition(arr,left,right);
+
+            quickSort(arr,left,pivot-1);
+            quickSort(arr,pivot+1,right);
+        }
+    }
+
+    public static int Partition(int[] arr, int left, int right)
+    {
+        int pivot = arr[right];
+        int i = left-1;
+
+        for (int j = left; j <right;j++)
+        {
+            if (arr[j]<=pivot)
+            {
+                i++;
+                swap(arr,i,j);
+            }
+        }
+        swap(arr,i+1,right);
+        return i+1;
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 
 }
